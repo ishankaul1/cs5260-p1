1d601a444c1b7c55ee294c9e588e27039ce40c94
Ishan Kaul <ishankaul2000@gmail.com>
2022-02-05 13:46:01 -0800

    most of work on parsing input files into usable data structures complete

--- src/schedule_driver.py
+++ src/schedule_driver.py
@@ -1,5 +1,14 @@
 #Runs parses input files and runs one instance of schedule_optimizer from the command line
+import resource
 import sys
+from os import path
+import csv
+from tokenize import String
+
+
+def err(msg: String):
+    print('ERROR: ' + msg)
+    sys.exit(1)
 
 usage = "driver.py <initial state filepath> <resource weight filepath> <output schedule filepath> <optimizing country name> <max depth> <# output schedules> <maximum frontier size>"
 
@@ -7,7 +16,7 @@ if len(sys.argv) != 8:
     print(usage)
     sys.exit(1)
 
-state_file, resource_file, output_file, my_country = sys.argv[1:5]
+state_filename, resources_filename, output_filename, my_country = sys.argv[1:5]
 max_depth, num_output_schedules, max_frontier_size = [int(x) for x in sys.argv[5:]]
 
 #sanity check the ints
@@ -21,7 +30,80 @@ if max_frontier_size < 1:
     print("Max frontier size must be a number greater than 1")
     sys.exit(1)
 
-#sanity check the filepaths and country name
+#sanity check the filepaths
+if not path.isfile(state_filename):
+    err("INIT STATE file '" + state_filename + "' does not exist")
+    
+elif not len(state_filename) > 4 and state_filename[-4:] == '.csv':
+    err("INIT STATE file '" + state_filename + "' is not a csv")
+
+
+if not path.isfile(resources_filename):
+    err("RESOURCE file '" + resources_filename + "' does not exist")
+    
+elif not len(resources_filename) > 4 and resources_filename[-4:] == '.csv':
+    err("INIT STATE file '" + resources_filename + "' is not a csv")
+    
+
+resources_file = open(resources_filename)
+state_file = open(state_filename)
+
+#parse resources and init state
+#resources: DICT {key='resource_name', val=(int)'weight'}
+#init_state: DICT {key='country_name', val={key='resource_name': val=(int)current_amount}}
+resources = {}
+init_state = {}
+
+#read resource file
+resources_reader = csv.reader(resources_file)
+resources_header = next(resources_reader)
+resources_rows = []
+
+for row in resources_reader:
+    resources_rows.append(row)
+
+#print(resources_header) #debug
+#print(resources_rows) #debug statement
+
+if len(resources_rows) != 1:
+    err('Should only be one row in resource weight file ' + resources_filename)
+
+#build resource dict
+for i in range(len(resources_header)):
+    try:
+        resources[resources_header[i]] = int(resources_rows[0][i])
+    except:
+        if resources_rows[0][i].strip() != 'x':
+            err('Resource weights can only be int or "x"')
+        else:
+            resources[resources_header[i]] = resources_rows[0][i].strip()
+
+#print(resources) #debug
+
+#read init state file
+state_reader = csv.reader(state_file)
+state_header = next(state_reader)
+state_rows = []
+
+for row in state_reader:
+    state_rows.append(row)
+
+#Ensure state file and resource file resources are the same
+if (set(state_header[1:]) != set(resources_header)):
+    err('Please ensure resources in resources file "' + resources_filename + '" and state file "' + state_filename + '" match.')
+
+
+
+#build state dict: TODO
+for i in range(len(resources_header)):
+    try:
+        resources[resources_header[i]] = int(resources_rows[0][i])
+    except:
+        if resources_rows[0][i].strip() != 'x':
+            err('Resource weights can only be int or "x"')
+        else:
+            resources[resources_header[i]] = resources_rows[0][i].strip()
+#sanity check - country name exists in state structure
 
 
 #initialize and run scheduler
