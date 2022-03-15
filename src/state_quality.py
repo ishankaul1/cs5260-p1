29e49e4452334fe616d2023cb3214815cc7ff1a5
Ishan Kaul <ishankaul2000@gmail.com>
2022-03-15 00:26:20 -0700

    most of state quality function implemented

--- src/state_quality.py
+++ src/state_quality.py
@@ -3,6 +3,42 @@
 #one country has at the time. The state quality function should output a value representative of how 'good' the current
 #state of that country is based on the resources
 
+import math
+
 #the most naive version possible, just for testing funzies
 def state_quality_basic(resources: dict)-> float:
     return sum(resources.values())
+
+def state_quality_realistic(resources: dict) -> float:
+    if resources['Population'] <= 0:
+        return 0 #can't have no people, worst country ever
+
+    #the real essentials - people, water, food, shelter
+    population_baseline_factor = -20 if resources['Population'] < 5 else 0
+
+    #TODO: find a better way to normalize the minimum for these 3
+    #idea: add factor 0.0000000001 to each ratio instead of the minimum
+    water_per_person_factor = -50 if resources['Water'] == 0 else max((resources['Water'] / resources['Population']) * 10, 30)
+    food_factor = -30 if resources['Food'] == 0 else max(math.log(resources['Food'] / resources['Population']) * 8, 30)
+    housing_factor = -20 if resources['Housing'] == 0 else max(math.log(resources['Housing'] / resources['Population'], 3) * 7,
+                         30)  # terrible if low ratio, diminishing returns on too much
+
+    #secondary - PotentialFossilEnergyUsable, PotentialRenewableEnergyUsable, Electronics
+
+
+    wastespread = resources['Population'] + resources['AvailableLand']
+    #penalties for waste - PopulationWaste, FarmWaste, MetallicAlloysWaste, ElectronicsWaste, HousingWaste, FoodWaste, PotentialFossilEnergyWaste, PotentialRenewableEnergyWaste
+    population_waste_factor = (resources['PopulationWaste'] / wastespread) * -4
+    farm_waste_factor = (resources['FarmWaste'] / wastespread) * -3 #compostable :P
+    metallic_waste_factor = (resources['MetallicAlloysWaste'] / wastespread) * -6
+    electronics_waste_factor = (resources['MetallicAlloysWaste'] / wastespread) * -8
+    housing_waste_factor = (resources['HousingWaste'] / wastespread) * -4
+    food_waste_factor = (resources['FoodWaste'] / wastespread) * -1
+    potentialfossilenergy_waste_factor = (resources['PotentialFossilEnergyWaste'] / wastespread) * -8
+    potentialrenewableenergy_waste_factor = (resources['PotentialRenewableEnergyWaste'] / wastespread) * -6
+
+    #overpopulation factor
+    overpopulation_factor = 0 if resources['Population'] / resources['AvailableLand'] <= 5 else (resources['Population'] / resources['AvailableLand']) * -2
+
+
+
